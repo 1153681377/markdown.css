0db86998101dfff8afd5b2f82d3888e975449194
年糕小豆汤 <ooiss@qq.com>
2016-03-12 17:49:54 +0800

    update github styles

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var nib = require('nib')();
+var stylus = require('gulp-stylus');
+var ejs = require('gulp-ejs');
+var marked = require('gulp-markdown');
+
+gulp.task('stylus', function() {
+    return gulp.src('./src/*/*/*.styl')
+    .pipe(stylus({use: nib}))
+    .pipe(gulp.dest('./public'));
+
+});
+
+gulp.task('markdown', function() {
+    return gulp.src('./src/markdown/markdown.md')
+    .pipe(marked())
+    .pipe(gulp.dest('./temp'));
+});
+
+gulp.task('ejs', ['markdown', 'stylus'], function() {
+    return gulp.src('./src/ejs/example-github.ejs')
+    .pipe(ejs({}, {ext: 'html'}))
+    .pipe(gulp.dest('./public'));
+});
