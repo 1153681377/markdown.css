cb25ba24a1299d41c92132cb72f3f88b3bc7b795
年糕小豆汤 <ooiss@qq.com>
2016-03-12 18:45:09 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,14 @@ gulp.task('markdown', function() {
 
 gulp.task('ejs', ['markdown', 'stylus'], function() {
     return gulp.src('./src/ejs/example-github.ejs')
-    .pipe(ejs({}, {ext: 'html'}))
+    .pipe(ejs({}, {ext: '.html'}))
     .pipe(gulp.dest('./public'));
+});
+
+gulp.task('public', ['ejs']);
+
+gulp.task('build', function() {
+    return gulp.src('./src/styles/*/*.styl')
+    .pipe(stylus({use: nib}))
+    .pipe(gulp.dest('./dest'));
 });
